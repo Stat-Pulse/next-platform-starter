e65e553f7f72de37a5cc59d8264d0de1818f656f
Stat-Pulse <sports@statpulseanalytics.com>
2025-05-09 16:48:04 -0500

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,12 +1,16 @@
 // pages/_document.js
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        {/* Load Tailwind from the official CDN */}
-        <script src="https://cdn.tailwindcss.com"></script>
+        {/* Load Tailwind from CDN before React hydrates */}
+        <Script
+          src="https://cdn.tailwindcss.com"
+          strategy="beforeInteractive"
+        />
       </Head>
       <body>
         <Main />
