8c85af90cf97375e08122905c07de75bcde4860b
Stat-Pulse <sports@statpulseanalytics.com>
2025-05-09 17:26:37 -0500

    Create players.js

---
+++ pages/players.js
@@ -0,0 +1,14 @@
+// pages/players.js
+import Header from '../components/Header'
+import PlayerSearch from '../components/PlayerSearch'
+import Footer from '../components/Footer'
+
+export default function Players() {
+  return (
+    <>
+      <Header />
+      <PlayerSearch />
+      <Footer />
+    </>
+  )
+}
