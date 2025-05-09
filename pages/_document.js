8e6978e173d080781e8a7d6d8d9219b06af41a13
Stat-Pulse <sports@statpulseanalytics.com>
2025-05-09 16:37:02 -0500

    Create _document.js

---
+++ pages/_document.js
@@ -0,0 +1,17 @@
+// pages/_document.js
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        {/* Load Tailwind from the official CDN */}
+        <script src="https://cdn.tailwindcss.com"></script>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
