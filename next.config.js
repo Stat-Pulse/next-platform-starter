ba4ebb02e72b1e8e875e9a3f547b09857ddaf495
Stat-Pulse <sports@statpulseanalytics.com>
2025-05-09 10:36:22 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: true
-};
-
-module.exports = nextConfig;
+module.exports = {
+  reactStrictMode: true,
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader', 'postcss-loader'],
+    });
+    return config;
+  },
+}
