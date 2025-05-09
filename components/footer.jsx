fe805c1795854b1be72ef26ef441f5e40ca85c5f
Stat-Pulse <sports@statpulseanalytics.com>
2025-05-09 10:37:37 -0500

    Update footer.jsx

--- components/footer.jsx
+++ components/footer.jsx
@@ -1,16 +1,7 @@
-import Link from 'next/link';
-
-export function Footer() {
-    return (
-        <footer className="pt-16 pb-12 sm:pt-24 sm:pb-16">
-            <p className="text-sm">
-                <Link
-                    href="https://docs.netlify.com/frameworks/next-js/overview/"
-                    className="decoration-dashed text-primary underline-offset-8"
-                >
-                    Next.js on Netlify
-                </Link>
-            </p>
-        </footer>
-    );
+export default function Footer() {
+  return (
+    <footer className="bg-gray-800 text-white p-4 text-center">
+      <p>&copy; 2025 StatPulse Analytics. All rights reserved.</p>
+    </footer>
+  );
 }
