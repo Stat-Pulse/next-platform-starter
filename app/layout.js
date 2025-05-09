99a24bc34fb9f49529e6ea9f1d08480ffeb01ccc
Stat-Pulse <sports@statpulseanalytics.com>
2025-05-09 15:27:33 -0500

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,12 @@
 import '../styles/globals.css'
 
-export const metadata = {
-  title: 'StatPulse',
-  description: '…'
-}
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+-     <body>{children}</body>
++     <body style={{ outline: '5px dashed red', backgroundColor: 'hotpink' }}>
+        {children}
+      </body>
     </html>
   )
 }
