556374324c8a27928dc772f26193448b193ccfaa
Stat-Pulse <sports@statpulseanalytics.com>
2025-05-12 11:51:28 -0500

    Update media-vault.js

--- pages/media-vault.js
+++ pages/media-vault.js
@@ -1,18 +1,30 @@
-import SectionWrapper from '/components/SectionWrapper'
+'use client'
+
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+import SidebarNavigation from '../components/SidebarNavigation'
+import SectionWrapper from '../components/SectionWrapper'
 
 export default function MediaVault() {
   return (
-    <SectionWrapper title="Media Vault">
-      <p className="text-gray-600 mb-4">Highlight videos and press conferences</p>
-      <div className="aspect-video">
-        <iframe
-          className="w-full h-full rounded"
-          src="https://www.youtube.com/embed/VIDEO_ID_HERE"
-          title="NFL Highlight"
-          frameBorder="0"
-          allowFullScreen
-        />
-      </div>
-    </SectionWrapper>
+    <>
+      <Header />
+      <main className="bg-gray-100 py-10">
+        <div className="container mx-auto px-6 grid grid-cols-1 md:grid-cols-4 gap-6">
+          <aside className="md:col-span-1">
+            <SidebarNavigation active="media-vault" />
+          </aside>
+
+          <div className="md:col-span-3 space-y-12">
+            <SectionWrapper title="Media Vault">
+              <p className="text-gray-600">
+                Watch game highlights, player interviews, podcasts, and exclusive NFL content.
+              </p>
+            </SectionWrapper>
+          </div>
+        </div>
+      </main>
+      <Footer />
+    </>
   )
 }
