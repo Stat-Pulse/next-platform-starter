34550f3f5caeb1bd18696d4699f54e0d8d76e065
Stat-Pulse <sports@statpulseanalytics.com>
2025-05-12 09:48:18 -0500

    Create teams.js

---
+++ data/teams.js
@@ -0,0 +1,260 @@
+const teams = [
+  {
+    name: "Arizona Cardinals",
+    slug: "cardinals",
+    abbreviation: "ARI",
+    conference: "NFC",
+    division: "West",
+    logo: "/team-logos/cardinals.png"
+  },
+  {
+    name: "Atlanta Falcons",
+    slug: "falcons",
+    abbreviation: "ATL",
+    conference: "NFC",
+    division: "South",
+    logo: "/team-logos/falcons.png"
+  },
+  {
+    name: "Baltimore Ravens",
+    slug: "ravens",
+    abbreviation: "BAL",
+    conference: "AFC",
+    division: "North",
+    logo: "/team-logos/ravens.png"
+  },
+  {
+    name: "Buffalo Bills",
+    slug: "bills",
+    abbreviation: "BUF",
+    conference: "AFC",
+    division: "East",
+    logo: "/team-logos/bills.png"
+  },
+  {
+    name: "Carolina Panthers",
+    slug: "panthers",
+    abbreviation: "CAR",
+    conference: "NFC",
+    division: "South",
+    logo: "/team-logos/panthers.png"
+  },
+  {
+    name: "Chicago Bears",
+    slug: "bears",
+    abbreviation: "CHI",
+    conference: "NFC",
+    division: "North",
+    logo: "/team-logos/bears.png"
+  },
+  {
+    name: "Cincinnati Bengals",
+    slug: "bengals",
+    abbreviation: "CIN",
+    conference: "AFC",
+    division: "North",
+    logo: "/team-logos/bengals.png"
+  },
+  {
+    name: "Cleveland Browns",
+    slug: "browns",
+    abbreviation: "CLE",
+    conference: "AFC",
+    division: "North",
+    logo: "/team-logos/browns.png"
+  },
+  {
+    name: "Dallas Cowboys",
+    slug: "cowboys",
+    abbreviation: "DAL",
+    conference: "NFC",
+    division: "East",
+    logo: "/team-logos/cowboys.png"
+  },
+  {
+    name: "Denver Broncos",
+    slug: "broncos",
+    abbreviation: "DEN",
+    conference: "AFC",
+    division: "West",
+    logo: "/team-logos/broncos.png"
+  },
+  {
+    name: "Detroit Lions",
+    slug: "lions",
+    abbreviation: "DET",
+    conference: "NFC",
+    division: "North",
+    logo: "/team-logos/lions.png"
+  },
+  {
+    name: "Green Bay Packers",
+    slug: "packers",
+    abbreviation: "GB",
+    conference: "NFC",
+    division: "North",
+    logo: "/team-logos/packers.png"
+  },
+  {
+    name: "Houston Texans",
+    slug: "texans",
+    abbreviation: "HOU",
+    conference: "AFC",
+    division: "South",
+    logo: "/team-logos/texans.png"
+  },
+  {
+    name: "Indianapolis Colts",
+    slug: "colts",
+    abbreviation: "IND",
+    conference: "AFC",
+    division: "South",
+    logo: "/team-logos/colts.png"
+  },
+  {
+    name: "Jacksonville Jaguars",
+    slug: "jaguars",
+    abbreviation: "JAX",
+    conference: "AFC",
+    division: "South",
+    logo: "/team-logos/jaguars.png"
+  },
+  {
+    name: "Kansas City Chiefs",
+    slug: "chiefs",
+    abbreviation: "KC",
+    conference: "AFC",
+    division: "West",
+    logo: "/team-logos/chiefs.png"
+  },
+  {
+    name: "Las Vegas Raiders",
+    slug: "raiders",
+    abbreviation: "LV",
+    conference: "AFC",
+    division: "West",
+    logo: "/team-logos/raiders.png"
+  },
+  {
+    name: "Los Angeles Chargers",
+    slug: "chargers",
+    abbreviation: "LAC",
+    conference: "AFC",
+    division: "West",
+    logo: "/team-logos/chargers.png"
+  },
+  {
+    name: "Los Angeles Rams",
+    slug: "rams",
+    abbreviation: "LAR",
+    conference: "NFC",
+    division: "West",
+    logo: "/team-logos/rams.png"
+  },
+  {
+    name: "Miami Dolphins",
+    slug: "dolphins",
+    abbreviation: "MIA",
+    conference: "AFC",
+    division: "East",
+    logo: "/team-logos/dolphins.png"
+  },
+  {
+    name: "Minnesota Vikings",
+    slug: "vikings",
+    abbreviation: "MIN",
+    conference: "NFC",
+    division: "North",
+    logo: "/team-logos/vikings.png"
+  },
+  {
+    name: "New England Patriots",
+    slug: "patriots",
+    abbreviation: "NE",
+    conference: "AFC",
+    division: "East",
+    logo: "/team-logos/patriots.png"
+  },
+  {
+    name: "New Orleans Saints",
+    slug: "saints",
+    abbreviation: "NO",
+    conference: "NFC",
+    division: "South",
+    logo: "/team-logos/saints.png"
+  },
+  {
+    name: "New York Giants",
+    slug: "giants",
+    abbreviation: "NYG",
+    conference: "NFC",
+    division: "East",
+    logo: "/team-logos/giants.png"
+  },
+  {
+    name: "New York Jets",
+    slug: "jets",
+    abbreviation: "NYJ",
+    conference: "AFC",
+    division: "East",
+    logo: "/team-logos/jets.png"
+  },
+  {
+    name: "Philadelphia Eagles",
+    slug: "eagles",
+    abbreviation: "PHI",
+    conference: "NFC",
+    division: "East",
+    logo: "/team-logos/eagles.png"
+  },
+  {
+    name: "Pittsburgh Steelers",
+    slug: "steelers",
+    abbreviation: "PIT",
+    conference: "AFC",
+    division: "North",
+    logo: "/team-logos/steelers.png"
+  },
+  {
+    name: "San Francisco 49ers",
+    slug: "49ers",
+    abbreviation: "SF",
+    conference: "NFC",
+    division: "West",
+    logo: "/team-logos/49ers.png"
+  },
+  {
+    name: "Seattle Seahawks",
+    slug: "seahawks",
+    abbreviation: "SEA",
+    conference: "NFC",
+    division: "West",
+    logo: "/team-logos/seahawks.png"
+  },
+  {
+    name: "Tampa Bay Buccaneers",
+    slug: "buccaneers",
+    abbreviation: "TB",
+    conference: "NFC",
+    division: "South",
+    logo: "/team-logos/buccaneers.png"
+  },
+  {
+    name: "Tennessee Titans",
+    slug: "titans",
+    abbreviation: "TEN",
+    conference: "AFC",
+    division: "South",
+    logo: "/team-logos/titans.png"
+  },
+  {
+    name: "Washington Commanders",
+    slug: "commanders",
+    abbreviation: "WAS",
+    conference: "NFC",
+    division: "East",
+    logo: "/team-logos/commanders.png"
+  }
+]
+
+export default teams
