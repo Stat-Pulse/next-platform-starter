65cbf82f7580dc1cc4b61168fdb3f560aa9b80e4
Stat-Pulse <sports@statpulseanalytics.com>
2025-05-12 11:00:18 -0500

    Create analytics-lab.js

---
+++ pages/analytics-lab.js
@@ -0,0 +1,9 @@
+import SectionWrapper from '@/components/SectionWrapper'
+
+export default function AnalyticsLab() {
+  return (
+    <SectionWrapper title="Analytics Lab">
+      <p className="text-gray-600">Advanced metrics, charts, and data tools coming soon.</p>
+    </SectionWrapper>
+  )
+}
