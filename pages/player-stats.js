47eba94cbd9fca7b361a38ae9a7c0970bac5023f
Stat-Pulse <sports@statpulseanalytics.com>
2025-05-14 12:26:48 -0500

    Update player-stats.js

--- pages/player-stats.js
+++ pages/player-stats.js
@@ -3,8 +3,8 @@
 import { useSearchParams } from 'next/navigation'
 import dynamic from 'next/dynamic'
 
-const PlayerStatsClient = dynamic(
-  () => import('../components/fantasy/PlayerStatsClient'),
+const PlayerProfile = dynamic(
+  () => import('../components/fantasy/PlayerProfile'),
   { ssr: false }
 )
 
@@ -12,5 +12,5 @@ export default function PlayerStatsPage() {
   const searchParams = useSearchParams()
   const playerId = searchParams.get('player_id')
 
-  return <PlayerStatsClient playerId={playerId} />
+  return <PlayerProfile playerId={playerId} />
 }
