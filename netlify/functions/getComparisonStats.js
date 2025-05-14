0ed666b048a217be1b24a9a53adb9bf85ba5b9af
Stat-Pulse <sports@statpulseanalytics.com>
2025-05-14 16:56:17 -0500

    Create getComparisonStats.js

---
+++ netlify/functions/getComparisonStats.js
@@ -0,0 +1,12 @@
+[
+  {
+    "player_id": "00-003123",
+    "player_name": "Josh Allen",
+    "fantasyPoints": 345.8,
+    "tds": 35,
+    "passYards": 4300,
+    "wins": 11,
+    "proBowl": true
+  },
+  ...
+]
