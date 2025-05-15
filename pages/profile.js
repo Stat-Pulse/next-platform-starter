505e5c266002aa70a52b4a0d28ff72320b8cd393
Stat-Pulse <sports@statpulseanalytics.com>
2025-05-15 09:22:36 -0500

    Update profile.js

--- pages/profile.js
+++ pages/profile.js
@@ -265,8 +265,13 @@ export default function ProfilePage() {
                   </div>
                   <div className="flex justify-end">
                     <button type="submit" className="bg-red-600 text-white px-4 py-2 rounded hover:bg-red-700">Save Changes</button>
-                  </div>
                   </form>
-                  </div>
-                  </div>
-                  )}
+                </div>
+              </div>
+            )}
+          </div>
+        </main>
+        <Footer />
+      </>
+    )
+  } 
