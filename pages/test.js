82eda9e1164023e5ebe28e106b278e35e1394265
Stat-Pulse <sports@statpulseanalytics.com>
2025-05-26 13:23:16 -0500

    Create test.js

---
+++ pages/test.js
@@ -0,0 +1,8 @@
+// pages/test.js
+export default function TestPage() {
+  return (
+    <div style={{ padding: '3rem', textAlign: 'center', color: 'blue' }}>
+      <h1 style={{ fontSize: '2rem' }}>✅ If you see this, test route is working</h1>
+    </div>
+  );
+}
