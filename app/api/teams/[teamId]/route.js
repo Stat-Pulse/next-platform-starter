9a780daba176ad837147994ba74a65bc3bfe5046
Stat-Pulse <sports@statpulseanalytics.com>
2025-05-30 05:42:19 -0500

    Update route.js

--- app/api/teams/[teamId]/route.js
+++ app/api/teams/[teamId]/route.js
@@ -17,9 +17,9 @@ export async function GET(request, { params }) {
     // Fetch team data by nickname (case-insensitive)
     const [teamRows] = await connection.execute(
       `SELECT team_abbr, team_name, team_division AS division, team_conf AS conference, team_logo_espn AS logo_url, 
-              team_color, team_color2, city, team_nick, stadium_name, head_coach, founded_year
+              primary_color, secondary_color, city, nickname, stadium_name, head_coach, founded_year
        FROM Teams
-       WHERE LOWER(team_nick) = ?`,
+       WHERE LOWER(nickname) = ?`,
       [teamIdLower]
     );
 
