89c73b9f49d484c60fdd66709c32a5caa22f3a3a
Jacob Fowler <jacobfowler@Mac.lan>
2025-06-07 16:18:52 -0500

    Push all updates

--- pages/api/player/[id].js
+++ pages/api/player/[id].js
@@ -6,10 +6,10 @@ export default async function handler(req, res) {
   try {
     console.log("Connecting to DB...");
     const connection = await mysql.createConnection({
-      host: process.env.MYSQL_HOST,
-      user: process.env.MYSQL_USER,
-      password: process.env.MYSQL_PASSWORD,
-      database: process.env.MYSQL_DATABASE,
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       connectTimeout: 10000  // 10 second timeout
     });
 
