a35ffeb00f949bf37a69dd7006f7ba85a941698b
Jacob Fowler <jacobfowler@Mac.lan>
2025-06-08 00:43:26 -0500

    Push all updates

--- pages/api/player/[id].js
+++ pages/api/player/[id].js
@@ -86,10 +86,34 @@ const [passingCareer] = await connection.execute(`
 player[0].career = receivingCareer[0];
 player[0].rushingCareer = rushingCareer[0];
 player[0].passingCareer = passingCareer[0];
+
+const seasonStatsQuery = `
+  SELECT season, 
+         SUM(receiving_yards) AS receiving_yards,
+         SUM(receiving_tds) AS receiving_tds,
+         SUM(rushing_yards) AS rushing_yards,
+         SUM(rushing_tds) AS rushing_tds,
+         SUM(passing_yards) AS passing_yards,
+         SUM(passing_tds) AS passing_tds,
+         SUM(interceptions) AS interceptions
+  FROM (
+    ${[...Array(15).keys()].map(i => {
+      const year = 2010 + i;
+      return `SELECT season, receiving_yards, receiving_tds, rushing_yards, rushing_tds, passing_yards, passing_tds, interceptions
+              FROM Player_Stats_${year}
+              WHERE player_id = ?`;
+    }).join('\nUNION ALL\n')}
+  ) AS combined
+  GROUP BY season
+  ORDER BY season ASC
+`;
+
+const [seasonStats] = await connection.query(seasonStatsQuery, Array(15).fill(req.query.id));
+
     console.log("Query complete. Sending response...");
     res.status(200).json({
       player: player[0],
-      seasonStats: [],
+      seasonStats,
       receivingMetrics: [],
       rushingMetrics: [],
       passingMetrics: [],
