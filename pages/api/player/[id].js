ab41b3189e19dffb1acfb551826d7e3e6b9e4eae
Jacob Fowler <jacobfowler@Mac.lan>
2025-06-07 22:45:33 -0500

    Push all updates

--- pages/api/player/[id].js
+++ pages/api/player/[id].js
@@ -15,7 +15,37 @@ export default async function handler(req, res) {
 
     console.log("Running player query...");
     const [player] = await connection.execute(
-      'SELECT * FROM Active_Player_Profiles WHERE player_id = ?',
+      `
+      SELECT
+        p.player_id,
+        p.player_name,
+        p.position,
+        p.college,
+        p.draft_year,
+        p.draft_team,
+        p.draft_round,
+        p.draft_overall,
+        p.date_of_birth,
+        p.height_inches,
+        p.weight_pounds,
+        p.is_active,
+        r.team AS team_abbr,
+        p.headshot_url,
+        lc.year_signed      AS contract_year,
+        lc.years            AS contract_length,
+        lc.value            AS contract_value,
+        lc.apy              AS contract_apy,
+        lc.guaranteed       AS contract_guaranteed,
+        lc.apy_cap_pct      AS contract_apy_cap_pct,
+        t.primary_color,
+        t.secondary_color,
+        t.team_logo_espn
+      FROM Players p
+      LEFT JOIN Rosters_2025 r ON p.player_id = r.gsis_id
+      LEFT JOIN Latest_Contracts lc ON p.player_id = lc.player_id
+      LEFT JOIN Teams t ON r.team = t.team_abbr
+      WHERE p.player_id = ?
+      `,
       [req.query.id]
     );
 
