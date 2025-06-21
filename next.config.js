de58daeff06b3340a765c7e192d6f42a4b09002f
Stat-Pulse <sports@statpulseanalytics.com>
2025-06-21 10:00:11 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,29 @@
-module.exports = {
+// next.config.js
+const path = require('path');
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+module.exports = withBundleAnalyzer({
   reactStrictMode: true,
-};
+  output: 'standalone', // Enables SSR routing support for Netlify
+  experimental: {
+    serverActions: { enabled: false }, // Disable server actions
+  },
+  images: {
+    domains: ['espn.com', 'wikipedia.org', 'localhost'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
+  webpack: (config, { isServer }) => {
+    config.resolve.alias['@'] = path.resolve(__dirname);
+    if (isServer) {
+      config.externals.push({ net: 'commonjs net', tls: 'commonjs tls' });
+    }
+    return config;
+  },
+});
