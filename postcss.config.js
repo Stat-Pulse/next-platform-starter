edcc329c066489052ef3d81088e674757ef5f788
Jacob Fowler <jacobfowler@Mac.lan>
2025-06-22 18:56:07 -0500

    Push all updates

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,6 @@
-// postcss.config.js  (CommonJS)
 module.exports = {
   plugins: {
-    '@tailwindcss': {},   // NEW – replaces 'tailwindcss'
-    autoprefixer: {},            // keep if you still target older browsers
+    tailwindcss: {},
+    autoprefixer: {},
   },
 };
